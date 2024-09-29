82b36f8ce2da612dc8ae8cae64b7eb4d0590ef4c
Edmond1844 <pavel.rudenko1844@gmail.com>
2024-09-29 18:49:20 +0300

    добавил js toggle

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,17 @@
-let popup = document.querySelector('.popup-ask');
+let openBtn = document.querySelector('.section-work__btn');
+let popupModal = document.querySelector('.popup-ask');
 let button = document.querySelector('.popup-ask__icon');
 
-button.addEventListener('click', function() {
-    popup.classList.add('popup-close');
+openBtn.addEventListener('click', function() {
+    popupModal.classList.add('popup-ask-open');
 });
 
-let popupVideo = document.querySelector('.popup-video');
+button.addEventListener('click', function() {
+    popupModal.classList.add('popup-close');
+});
+
 let videoBtn = document.querySelector('.section-vidio-phone__button');
+let popupVideo = document.querySelector('.popup-video');
 let videoClose = document.querySelector('.popup-video__icon-btn');
 
 videoBtn.addEventListener('click', function() {
@@ -17,3 +22,15 @@ videoClose.addEventListener('click', function() {
     popupVideo.classList.remove('popup-video-open');
     popupVideo.classList.add('popup-close');
 });
+
+let buttons = document.querySelectorAll('.section-work__items');
+
+for (let button of buttons) {
+        button.addEventListener('click', function() {
+        button.classList.toggle('section-work__items--active');
+    });
+}
+
+// let span = document.querySelector('.section-work__title-more');
+// let visit = document.querySelector('.section-work__wrapper-more');
+
